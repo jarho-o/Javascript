309a8853ecb8fb6805aa8723815f306a8cf3f3c4
Jarho-o <adjarhodesmond@gmail.com>
2026-04-02 13:33:36 +0100

    Add index.js file

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 //A var that stores the name of a user via a prompt.
 var name = prompt("What is your name?");
 
-//Isolating the first characters
-var firstChar = name.slice(0,1);
+var newName = name.slice(0, 1).toUpperCase() + name.slice(1, name.length).toLowerCase();
 
-//First char to upper case
-var upperCaseFirstChar = firstChar.toUpperCase();
-
-//Isolating the remaining characters
-var secondChar = name.slice(1,name.length);
-
-//Ensuring the remaining char of the name remains in small letters
-var lowerCaseSecondChar = secondChar.toLowerCase();
-
-//capitalised version of name using concatenation
-var capitalisedName = upperCaseFirstChar + lowerCaseSecondChar
-
-alert("Welcome " + capitalisedName + "!");
+alert("Hello, " + newName + "! Welcome to JavaScript programming!");
